14f279a4960ee3be35db0646d4a5eab12b49c966
Shane <shanehill16@gmail.com>
2019-03-14 16:40:31 -0400

    Jawsdb fix

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 let connection;
 
-if(process.env.JAWSDBD_URL){
-  connection = mysql.createConnection(process.env.JAWSDBD_URL);
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
     host: "localhost",
